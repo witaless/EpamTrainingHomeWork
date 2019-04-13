2c80cd8aab99465fed4c570100a3e9c6ecb108ef
witaless <witaless99@gmail.com>
2019-04-13 14:32:20 +0300

    added build variants, free and paid versions of resources

---
+++ app/src/main/java/com/witaless/epamtraininghomework/MainActivity.java
@@ -0,0 +1,30 @@
+package com.witaless.epamtraininghomework;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle(R.string.app_name);
+
+        FloatingActionButton fab = findViewById(R.id.floating_action_bar);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, R.string.snackbar_text, Snackbar.LENGTH_LONG)
+                        .setAction(R.string.snackbar_action_text, null).show();
+            }
+        });
+    }
+
+}
